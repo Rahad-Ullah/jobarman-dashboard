1330374900e14b2799dc8a4c7b33743e4dcc61cc
Rahad Ullah <rahadullah10@gmail.com>
2025-11-29 12:05:55 +0600

    integrate: dashboard summary

--- src/app/(dashboard-layout)/page.tsx
+++ src/app/(dashboard-layout)/page.tsx
@@ -3,15 +3,19 @@ import { Card } from "@/components/ui/card";
 import { EarningChart } from "@/components/page/analytics/charts/EarningChart";
 import WeeklyReport from "@/components/page/analytics/charts/WeeklyReport";
 import ManualSearch from "@/components/page/analytics/charts/ManualSearch";
+import { nextFetch } from "@/utils/nextFetch";
 
-const AnalyticsPage = () => {
+const AnalyticsPage = async () => {
+  const summaryRes = await nextFetch("/dashboard/summury");
+  const summary = summaryRes?.data;
+
   return (
     <Card className="h-full bg-transparent border-none animate-fadeIn flex flex-col gap-6">
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
-        <StatCard title="Total Job Seeker" value="12,345" />
-        <StatCard title="Total Recruiter" value="550" />
-        <StatCard title="Active Jobs" value="200" />
-        <StatCard title="Total Revenue" value="$50,000" />
+        <StatCard title="Total Job Seeker" value={summary?.toalJobSeekers || 0} />
+        <StatCard title="Total Recruiter" value={summary?.toalRecruiters || 0} />
+        <StatCard title="Active Jobs" value={summary?.totalActiveJobs || 0} />
+        <StatCard title="Total Revenue" value={summary?.totalRevinue || 0} prefix="$"/>
       </div>
 
       <EarningChart />
--- src/components/page/analytics/cards/StatCard.tsx
+++ src/components/page/analytics/cards/StatCard.tsx
@@ -3,13 +3,15 @@ import React from "react";
 interface StatCardProps {
   title: string;
   value: string;
+  prefix?: string;
+  suffix?: string;
 }
 
-const StatCard: React.FC<StatCardProps> = ({ title, value }) => {
+const StatCard: React.FC<StatCardProps> = ({ title, value, prefix, suffix }) => {
   return (
     <div className="grid justify-items-center items-center gap-4 bg-white p-5 px-8 rounded-lg border hover:shadow-md transition-shadow duration-300">
       <h3 className="text-xl font-medium text-gray-950">{title}</h3>
-      <p className="text-4xl font-bold text-primary-foreground">{value}</p>
+      <p className="text-4xl font-bold text-primary-foreground">{prefix}{value}{}{suffix}</p>
     </div>
   );
 };
